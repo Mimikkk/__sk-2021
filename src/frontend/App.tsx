26d5558ef131c6ec4cc24f8474f934f1f5b2e961
Hououin Kyouma <inwazzin@gmail.com>
2022-01-01 01:20:49 +0100

    remove logger pointer effects

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -5,34 +5,40 @@ import { Logger } from './components/devtools/Logger';
 
 const server_url = import.meta.env.VITE_SERVER_URL;
 
-export const App = () => {
+export const ClientView = () => {
   const { socket, status } = useSocket(`ws://${server_url}`);
 
   return (
-    <>
-      <div className="app">
-        <header className="app-header">
-          <p>React</p>
-          <button
-            type="button"
-            onClick={() => {
-              console.log(status);
-              if (isDisconnected(status)) {
-                socket.onopen = () => console.log('Connected');
-                socket.onclose = () => console.log('Close');
-                socket.onmessage = console.log;
-                socket.onerror = console.log;
-              } else if (isConnected(status)) {
-                socket.close();
-              }
-            }}
-          >
-            Socket is {status}
-          </button>
-          <p>Socket on {socket.url}.</p>
-        </header>
-        <Logger />
-      </div>
-    </>
+    <div>
+      <button
+        type="button"
+        onClick={() => {
+          console.log(status);
+          if (isDisconnected(status)) {
+            socket.onopen = () => console.log('Connected');
+            socket.onclose = () => console.log('Close');
+            socket.onmessage = console.log;
+            socket.onerror = console.log;
+          } else if (isConnected(status)) {
+            socket.close();
+          }
+        }}
+      >
+        Socket is {status}
+      </button>
+      <p>Socket on {socket.url}.</p>
+    </div>
+  );
+};
+
+export const App = () => {
+  return (
+    <div className="app">
+      <header>Title</header>
+      <article>
+        <ClientView />
+      </article>
+      <Logger />
+    </div>
   );
 };
