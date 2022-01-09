f1e078b3fb3a8e3dd092c77a87322a7a11f9572b
Hououin Kyouma <inwazzin@gmail.com>
2022-01-09 04:17:46 +0100

    working responses

--- src/frontend/components/ClientView/ClientView.tsx
+++ src/frontend/components/ClientView/ClientView.tsx
@@ -1,7 +1,12 @@
-import React, { ChangeEvent, useEffect, useRef } from 'react';
-import { commandService } from 'services';
+import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
+import {
+  commandService,
+  ServerEvent,
+  ServerEventType,
+  ServerInfo,
+  ServerMessage,
+} from 'services';
 import faker from 'faker';
-import { isDisconnected, toStatus } from 'hooks/useSocket/socket-status';
 import { Nullable } from 'utils';
 import './ClientView.scss';
 
@@ -9,11 +14,20 @@ const server_url = import.meta.env.VITE_SERVER_URL;
 
 export const ClientView = () => {
   const socket = useRef<Nullable<WebSocket>>(null);
+  const [users, setUsers] = useState<string[]>([]);
 
   useEffect(() => {
     if (socket.current) {
       socket.current.onmessage = (event) => {
         console.log({ event });
+
+        let data = JSON.parse(event.data) as ServerEvent;
+        if (data.type === ServerEventType.Info) {
+          setUsers((data as ServerInfo).names);
+        } else if (data.type === ServerEventType.Message) {
+          let { messenger, message } = data as ServerMessage;
+          console.log(`Message from ${messenger} with ${message}`);
+        }
       };
       socket.current.onerror = (event) => {
         console.log({ event });
@@ -29,12 +43,8 @@ export const ClientView = () => {
 
   const [name, setName] = React.useState<string>('');
 
-  const handleChange = ({
-    target: { value },
-  }: ChangeEvent<HTMLInputElement>) => {
-    console.log(`Input change ${value}`);
+  const handleChange = ({ target: { value } }: ChangeEvent<HTMLInputElement>) =>
     setName(value);
-  };
 
   return (
     <div className="client-view">
@@ -45,35 +55,46 @@ export const ClientView = () => {
 
       <button
         onClick={() => {
+          console.log('creating new connection with server');
           socket.current = new WebSocket(`ws://${server_url}/${name}`);
         }}
-        disabled={
-          !isDisconnected(toStatus(socket.current?.readyState || 3)) || !name
-        }
+        // disabled={
+        //   !isDisconnected(toStatus(socket.current?.readyState || 3)) || !name
+        // }
       >
         Reconnect
       </button>
       <button
+        onClick={() => {
+          console.log('Closing connection with server');
+          commandService.close(socket.current!);
+          socket.current?.close();
+        }}
+        // disabled={!isConnected(toStatus(socket.current?.readyState || 3))}
+      >
+        Disconnect
+      </button>
+      <button
         type="button"
         onClick={() => {
-          commandService.sendMessage(
+          commandService.send(
             socket.current!,
             faker.lorem.sentence(),
             'monika',
           );
           console.log('Send random message');
         }}
-        disabled={!socket.current}
+        // disabled={!socket.current}
       >
         Send random sentence
       </button>
       <button
         type="button"
         onClick={() => {
-          commandService.readServerInfo(socket.current!);
+          commandService.requestServerInfo(socket.current!);
           console.log('Requested server info');
         }}
-        disabled={!socket.current}
+        // disabled={!socket.current}
       >
         Request server info
       </button>
--- src/frontend/services/command.service.ts
+++ src/frontend/services/command.service.ts
@@ -1,15 +1,43 @@
 export enum ServerCommand {
   Info = '<>server_info<>',
   Message = '<>message<>',
+  Close = '<>close_connection<>',
+}
+
+export enum ServerEventType {
+  Info = 'info',
+  Message = 'message',
+  Close = 'close',
+}
+
+export interface ServerEvent {
+  type: ServerEventType;
+}
+
+export interface ServerInfo extends ServerEvent {
+  names: string[];
+}
+
+export interface ServerMessage extends ServerEvent {
+  messenger: string;
+  message: string;
 }
 
 interface Service {
-  readServerInfo: (socket: WebSocket) => void;
-  sendMessage: (socket: WebSocket, message: string, recipient: string) => void;
+  requestServerInfo: (socket: WebSocket) => void;
+  send: (socket: WebSocket, message: string, recipient: string) => void;
+  close: (socket: WebSocket) => void;
 }
 
 export const commandService: Service = {
-  readServerInfo: (socket) => socket.send(ServerCommand.Info),
-  sendMessage: (socket, message, recipient) =>
-    socket.send(`${ServerCommand.Message}<${recipient}|${message}>`),
+  requestServerInfo(socket) {
+    socket.send(ServerCommand.Info);
+  },
+  send(socket, message, recipient) {
+    socket.send(`${ServerCommand.Message}<${recipient}|${message}>`);
+  },
+  close(socket) {
+    socket.send(ServerCommand.Close);
+    socket.close();
+  },
 };
